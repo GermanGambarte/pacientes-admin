837c5ea3d1a9cf7bceceb10e93cc1f8c587d0aa9
GermanGambarte <germangambarte@gmail.com>
2022-11-23 20:44:50 -0300

    add: inputs state magement

--- src/components/Form.component.jsx
+++ src/components/Form.component.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Form = () => {
+  const [date, setDate] = useState({
+    pet: '',
+    propietary: '',
+    email: '',
+    day: '',
+    hour: '',
+    symptoms: '',
+  })
+  const onChangeHandler = (e) => {
+    setDate({ ...date, [e.target.name]: e.target.value })
+  }
+
+  const { pet, propietary, email, day, hour, symptoms } = date
+
   return (
     <form className="Form">
       <h2 className="Form-h2">Crear nueva cita</h2>
@@ -8,7 +22,14 @@ export const Form = () => {
         <label className="Form-label" htmlFor="pet">
           Nombre de la Mascota:
         </label>
-        <input className="Form-input" id="pet" name="pet" type="text" />
+        <input
+          className="Form-input"
+          id="pet"
+          name="pet"
+          type="text"
+          value={pet}
+          onChange={onChangeHandler}
+        />
       </div>
       <div className="Form-div">
         <label className="Form-label" htmlFor="propietary">
@@ -19,25 +40,64 @@ export const Form = () => {
           id="propietary"
           name="propietary"
           type="text"
+          value={propietary}
+          onChange={onChangeHandler}
         />
       </div>
       <div className="Form-div">
         <label className="Form-label" htmlFor="email">
           Email:
         </label>
-        <input className="Form-input" id="email" name="email" type="email" />
+        <input
+          className="Form-input"
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={onChangeHandler}
+        />
       </div>
-      <div className="Form-div">
-        <label className="Form-label" htmlFor="discharged">
-          Alta:
-        </label>
-        <input className="Form-input" type="date" />
+      <div className="Form-div--date">
+        <div className="Form-div--day">
+          <label className="Form-label" htmlFor="day">
+            Fecha:
+          </label>
+          <input
+            className="Form-input"
+            id="day"
+            name="day"
+            type="date"
+            value={day}
+            onChange={onChangeHandler}
+          />
+        </div>
+        <div className="Form-div--time">
+          <label className="Form-label" htmlFor="hour">
+            Hora:
+          </label>
+          <input
+            className="Form-input"
+            id="hour"
+            name="hour"
+            type="time"
+            value={hour}
+            onChange={onChangeHandler}
+          />
+        </div>
       </div>
       <div>
         <label className="Form-label" htmlFor="symptoms">
           Sintomas:
         </label>
-        <textarea className="Form-textarea" cols="6" id="symptoms" rows="5" />
+        <textarea
+          className="Form-textarea"
+          cols="6"
+          id="symptoms"
+          name="symptoms"
+          rows="5"
+          value={symptoms}
+          onChange={onChangeHandler}
+        />
       </div>
       <p className="Form-error">Texto de error</p>
       <button className="Form-button" type="submit">
